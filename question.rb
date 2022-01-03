3730cc578bbe9d052f03dfb73f78ecc975c129b3
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-03 22:38:19 +0000

    add output for num generator

--- question.rb
+++ question.rb
@@ -1,9 +1,15 @@
 class Question
+  
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+  end
+
   def generate(name)
-    num1 = rand(1..20)
-    num2 = rand(1..20)
-    puts "#{name}: What does #{num1} plus #{num2} equal?"
-    answer = num1 + num2
+
+    puts "#{name}: What does #{@num1} plus #{@num2} equal?"
+    print '> '
+    answer = @num1 + @num2
     return answer
   end
 end
