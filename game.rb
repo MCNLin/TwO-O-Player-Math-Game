959139385ab90e97442c6b2aa42fbbb0e1e73f79
Mei Nghiem-Lin <mcnghiem@gmail.com>
2022-01-03 22:37:21 +0000

    add methods and behaviour for gameplay

--- game.rb
+++ game.rb
@@ -1,27 +1,50 @@
 require_relative './player'
 
+
 class Game
   def initialize
-    @player1 = Player.new(@name)
-    @player2 = Player.new(@name)
-
-    #shuffles player list so player 1 isn't always first
-    @players = [@player1, @player2].shuffle
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
   end
 
-  def winner
-    @players.last
+  
+  def stats
+    puts "🔊️ Player 1: #{@player1.lives}/3 vs Player 2: #{@player2.lives}/3"
+  end
+  
+  def check_score
+      if @player1.game_over
+        winner(@player2)
+      elsif @player2.game_over
+        winner(@player1)
+    end
   end
 
-  def game_over
-    @players.select {|player| player.loser?}.count > 0
+  def winner(player)
+    puts "🎊️ #{player.name} wins with the score of #{player.lives}/3 🎊️"
+    puts "\n"
+    puts '🛑️------------GAME-OVER------------🛑️'
+    puts 'Thanks for playing! Good bye!👋️'
+    exit(0)
   end
-
+  
   def start
-    puts "Let's Play the TwO-O-Player Math Game!"
+    puts "\n"
+    puts "✨️ Let's Play the TwO-O-Player Math Game! ✨️"
+    puts "\n"
+    next_turn
+  end
 
-    
-    
+  def next_turn
+    @player1.question
+    check_score
+    @player2.question
+    check_score
+    stats
+    puts "\n"
+    puts '🌟️-------------NEW-TURN-------------🌟️'
+    puts "\n"
+    next_turn 
   end
 end
 
